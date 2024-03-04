6d72a25c4fb114bac9985b812697e1bfe90473dd
MvAalten <157478216+MvAalten@users.noreply.github.com>
2024-03-04 16:27:24 +0100

    i linked everything
    linked everything, set up for the correct way to log the exercises

--- pages/log.php
+++ pages/log.php
@@ -12,9 +12,28 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap" rel="stylesheet">
     <title>PUMP UP</title>
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="../css/main.css">
 </head>
 <body>
+    <div class="space">
 
+    </div>
+    <div class="header">
+        <div class="logo">
+            <h2>PUMP UP</h2>
+        </div>
+        <div class="buttons">
+            <a class="auto" id="log" href="../index.php">Return</a>
+            <a class="auto" id="trainers">Personal Trainer</a>
+            <a class="auto" id="workouts">Workouts</a>
+            <a class="auto" id="exercises  ">Exercises</a>
+            <a class="login-button">login</a>
+        </div>
+    </div>
+    <div class="under-body">
+        <div class="log-workout-box">
+            
+        </div>
+    </div>
 </body>
 </html>
