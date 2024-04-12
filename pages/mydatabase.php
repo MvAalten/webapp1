cc0e3ce9dfb53d6b000dde16c723cb3fb3c6d98a
MvAalten <157478216+MvAalten@users.noreply.github.com>
2024-04-12 21:19:58 +0200

    database added
    added database in php array

---
+++ pages/mydatabase.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.2.1
+ */
+
+/**
+ * Database `mydatabase`
+ */
+
+/* `mydatabase`.`exercises` */
+$exercises = array(
+  array('id' => '1','exerciseName' => 'Squat','muscle' => 'hamstrings'),
+  array('id' => '3','exerciseName' => 'barbell bench press','muscle' => 'chest'),
+  array('id' => '4','exerciseName' => 'cable front raise','muscle' => 'shoulder'),
+  array('id' => '5','exerciseName' => 'arnold press','muscle' => 'shoulder'),
+  array('id' => '6','exerciseName' => 'pulldown','muscle' => 'back'),
+  array('id' => '7','exerciseName' => 'dumbbell row','muscle' => 'back')
+);
+
+/* `mydatabase`.`users` */
+$users = array(
+  array('id' => '1','username' => 'mees','password' => '1','email' => 'mees@gmail.com'),
+  array('id' => '4','username' => 'h','password' => 'h','email' => 'h@h')
+);
